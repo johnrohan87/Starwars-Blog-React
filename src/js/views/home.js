af0fddc0d6fefdc863ed3e44657e581f36ee994f
John Rohan <john.rohan87@gmail.com>
2020-07-10 01:11:46 +0000

    updating

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,68 @@
-import React from "react";
+import React, { useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+function convertPeopleIntoHTML(people) {
+	//<p key={planet.name}>{planet.name}</p>;
+	return (
+		<div className="card">
+			<div className="card-body">
+				<img src="https://via.placeholder.com/400x200" />
+				<h5 className="card-title">{people.name}</h5>
+				<p className="card-text">
+					<div>Gender: {people.gender}</div>
+					<div>Hair Color: {people.hair_color}</div>
+					<div>Eye Color: {people.eye_color}</div>
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+}
+
+function convertPlanetIntoHTML(planet) {
+	//<p key={planet.name}>{planet.name}</p>;
+	return (
+		<div className="card">
+			<div className="card-body">
+				<img src="https://via.placeholder.com/400x200" />
+				<h5 className="card-title">{planet.name}</h5>
+				<p className="card-text">
+					<div>Population: {planet.population}</div>
+					<div>Terrain: {planet.terrain}</div>
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+}
+
+export const Home = () => {
+	const { actions, store } = useContext(Context);
+	return (
+		<div className="text-center mt-5">
+			<button
+				type="button"
+				onClick={() => {
+					actions.getPlanets();
+					actions.getPeople();
+				}}>
+				get API list
+			</button>
+			{store.planets.length == 0 ? <p>No planets</p> : ""}
+			<div className="w-100 people">
+				{store.planets.length == 0 ? "" : <h1 className="d-flex text-danger p4">Characters</h1>}
+				<div className="d-inline-flex">{store.people.map(convertPeopleIntoHTML)}</div>
+			</div>
+			<div className="w-100 planets">
+				{store.planets.length == 0 ? "" : <h1 className="d-flex text-danger p4">Planets</h1>}
+				<div className="d-inline-flex">{store.planets.map(convertPlanetIntoHTML)}</div>
+			</div>
+		</div>
+	);
+};
